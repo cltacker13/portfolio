8cb2c275a434bc3aa6694f3a698d285612e92f1f
Chrystal Tacker <cltacker13@gmail.com>
2024-01-15 14:35:52 -0500

    new background img, developer and freelance. nav and other edits.

--- NavBar.js
+++ NavBar.js
@@ -5,17 +5,19 @@ document.getElementById("navbar").innerHTML = `
     <div class="subnav">
         <button class="subnavbtn">Professional</button>
         <div class="subnav-content">
+            <a href='developer.html'>Developer</a>
+            <a href='entrepreneur.html'>Entrepreneur</a>
             <a href='employee.html'>Employee</a>
             <a href='freelance.html'>Freelance</a>
-            <a href='student.html'>Student</a>
         </div>
     </div> 
     <div class="subnav">
         <button class="subnavbtn">Personal</button>
         <div class="subnav-content">
-            <a href='build.html'>Builder</a>
+            <a href='build.html'>Carpenter</a>
             <a href='photograph.html'>Photographer</a>
+            <a href='student.html'>Student</a>
         </div>
     </div>       
     <a href='contact.html'>Contact</a>
-`;
+`; 
